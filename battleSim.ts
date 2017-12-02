ae05232f667ef4a99c6c5002142c0899a72c4f02
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2017-12-02 17:02:41 -0500

    V1.0.1 Abstract Teams

--- battleSim.ts
+++ battleSim.ts
@@ -11,7 +11,7 @@ class Unit {
     private speed : number;
     private damage : number;
     private range : number;
-    private team : number;
+    public team : number;
     constructor(x : number, y : number, health : number, speed : number, damage : number, range: number, team : number, sprite : string) {
         this.basicUnit = game.add.sprite(x, y, sprite);
         game.physics.arcade.enable(this.basicUnit);
@@ -56,23 +56,6 @@ class Unit {
         } else{
             throw "Programmer Error..."
         }
-    }
-
-    /**
-     *
-     * @param {Array<Unit>} arrayOfUnits
-     * @return {Array<Unit>}
-     */
-    filterForOtherTeams(arrayOfUnits : Array<Unit>) : Array<Unit> {
-        let currentUnitsTeam = this.team;
-        let otherTeams : Array<Unit> = [];
-
-        for(let i : number = 0; i < arrayOfUnits.length; i++) {
-            if(arrayOfUnits[i].team != currentUnitsTeam) {
-                otherTeams.push(arrayOfUnits[i])
-            }
-        }
-        return otherTeams
     }
 
     /**
@@ -187,8 +170,7 @@ function a(){
 var amountUnits : number = 50;
 var amountKnights : number = 10;
 var nightsAttacking : boolean = false;
-var basicUnitsT0 : Array<Unit> = [];
-var basicUnitsT1 : Array<Unit> = [];
+var allUnits : Array<Unit> = [];
 
 function create() {
 
@@ -213,7 +195,7 @@ function create() {
             y += 30;
             xdiff = 0;
         }
-        basicUnitsT0[i] = new Unit(250 + xdiff, y, 100, 100,0.95, 100, 0, 'basicUnit');
+        allUnits[i] = new Unit(250 + xdiff, y, 100, 100,0.95, 100, 0, 'basicUnit');
         xdiff += 25;
     }
 
@@ -229,7 +211,7 @@ function create() {
     }*/
 
     for(let i : number = 0; i < amountKnights; i++) {
-        basicUnitsT1[i] = new Unit(250 + i * 25, 400, 150, 60, 1, 60, 1, 'knightUnit');
+        allUnits[i] = new Unit(250 + i * 25, 400, 150, 60, 1, 60, 1, 'knightUnit');
     }
 
 
@@ -247,6 +229,42 @@ function create() {
     }*/
 
 
+}
+
+
+/**
+ *
+ * @param {Array<Unit>} arrayOfUnits
+ * @param teamNumber
+ * @return {Array<Unit>}
+ */
+function filterForOtherTeams(arrayOfUnits : Array<Unit>, teamNumber : number) : Array<Unit> {
+    let otherTeams : Array<Unit> = [];
+
+    for(let i : number = 0; i < arrayOfUnits.length; i++) {
+        if(arrayOfUnits[i].team != teamNumber) {
+            otherTeams.push(arrayOfUnits[i])
+        }
+    }
+    return otherTeams
+}
+
+
+/**
+ *
+ * @param {Array<Unit>} arrayOfUnits
+ * @param teamNumber
+ * @return {Array<Unit>}
+ */
+function filterForSameTeam(arrayOfUnits : Array<Unit>, teamNumber : number) : Array<Unit> {
+    let otherTeams : Array<Unit> = [];
+
+    for(let i : number = 0; i < arrayOfUnits.length; i++) {
+        if(arrayOfUnits[i].team === teamNumber) {
+            otherTeams.push(arrayOfUnits[i])
+        }
+    }
+    return otherTeams
 }
 
 /**
@@ -270,15 +288,15 @@ function filterUnitsAlive(basicUnits : Array<Unit>) {
 /**
  * Sets team teamNum on the move towards other teams
  *
+ * @param basicUnits
  * @param {number} teamNum
  */
-function teamMove(teamNum: number){
-    for(let unit of basicUnitsT0) {
-        unit.basicUnit.body.immovable = true;
-    }
+function teamMove(basicUnits : Array<Unit>, teamNum: number){
+    let basicUnitsFromOtherTeams = filterForOtherTeams(basicUnits, teamNum);
+    let basicUnitsFromTeam = filterForSameTeam(basicUnits, teamNum);
 
-    for(let unit of filterUnitsAlive(basicUnitsT1)) {
-        let closestUnit = unit.closestUnit(filterUnitsAlive(basicUnitsT0));
+    for(let unit of filterUnitsAlive(basicUnitsFromTeam)) {
+        let closestUnit = unit.closestUnit(filterUnitsAlive(basicUnitsFromOtherTeams));
 
         let closestUnitX = closestUnit.basicUnit.x;
         let closestUnitY = closestUnit.basicUnit.y;
@@ -305,72 +323,36 @@ function teamMove(teamNum: number){
     }
 }
 
+/**
+ * Function makes teamNum  attack everybody else
+ *
+ * @param basicUnits : Array<Units> all the units
+ * @param teamNum : number the attacking team
+ */
+function teamAttack(basicUnits : Array<Unit>, teamNum : number){
+    let basicUnitsFromOtherTeams = filterForOtherTeams(basicUnits, teamNum);
+    let basicUnitsFromTeam = filterForSameTeam(basicUnits, teamNum);
+    for(let unit of basicUnitsFromTeam) {
+        let closestUnit = unit.closestUnit(basicUnitsFromOtherTeams);
+        // Check if within range of night
+        if(unit.withinRange([closestUnit]).length > 0){
+            closestUnit.health = closestUnit.health - unit.getDamage();
+            if(closestUnit.health <= 0) {
+                closestUnit.killUnit();
+            }
+            //console.log(closestUnit.health);
+        }
+    }
+}
+
 function handleMovement(){
 
 
-    if(nightsAttacking) {
-        for(let unit of basicUnitsT0) {
-            unit.basicUnit.body.immovable = true;
-        }
-
-        for(let unit of filterUnitsAlive(basicUnitsT1)) {
-            let closestUnit = unit.closestUnit(filterUnitsAlive(basicUnitsT0));
-
-            let closestUnitX = closestUnit.basicUnit.x;
-            let closestUnitY = closestUnit.basicUnit.y;
-
-            let unitX = unit.basicUnit.x;
-            let unitY = unit.basicUnit.y;
-
-            if(closestUnitX > unitX) {
-                /*for(var i = 0; i < nightUnits.length; i++) {
-                    nightUnits[i].nightUnit.speed
-                }*/
-                /* do not try to play with the velocity of the sprite's body in here */
-                //night.nightUnit.body.speed =
-                unit.setDirectionX(1)
-            } else {
-                unit.setDirectionX(-1)
-            }
-
-            if(closestUnitY > unitY) {
-                unit.setDirectionY(1)
-            } else {
-                unit.setDirectionY(-1)
-            }
-        }
-    }else{
-        for(let unit of basicUnitsT1) {
-            unit.basicUnit.body.immovable = true;
-        }
-
-        for(let unit of filterUnitsAlive(basicUnitsT0)) {
-            let closestUnit = unit.closestUnit(filterUnitsAlive(basicUnitsT1));
-
-            let closestUnitX = closestUnit.basicUnit.x;
-            let closestUnitY = closestUnit.basicUnit.y;
-
-            let unitX = unit.basicUnit.x;
-            let unitY = unit.basicUnit.y;
-
-            if(closestUnitX > unitX) {
-                /*for(var i = 0; i < nightUnits.length; i++) {
-                    nightUnits[i].nightUnit.speed
-                }*/
-                /* do not try to play with the velocity of the sprite's body in here */
-                //night.nightUnit.body.speed =
-                unit.setDirectionX(1)
-            } else {
-                unit.setDirectionX(-1)
-            }
-
-            if(closestUnitY > unitY) {
-                unit.setDirectionY(1)
-            } else {
-                unit.setDirectionY(-1)
-            }
-        }
-    }
+    //if(nightsAttacking) {
+        teamMove(allUnits, 0);
+    //}else{
+        teamMove(allUnits, 1);
+    //}
 }
 
 function update() {
@@ -380,36 +362,11 @@ function update() {
 
     /* Attack Logic begin*/
 
-
-
-
-    for(let unit of basicUnitsT1) {
-        let closestUnit = unit.closestUnit(basicUnitsT0);
-        // Check if within range of night
-        if(unit.withinRange([closestUnit]).length > 0){
-            closestUnit.health = closestUnit.health - unit.getDamage();
-            if(closestUnit.health <= 0) {
-                closestUnit.killUnit();
-            }
-            //console.log(closestUnit.health);
-        }
-    }
-
-
-    for(let unit of basicUnitsT0) {
-        let closestUnit = unit.closestUnit(basicUnitsT1);
-        // Check if within range of night
-        if(unit.withinRange([closestUnit]).length > 0){
-            closestUnit.health = closestUnit.health - unit.getDamage();
-            if(closestUnit.health <= 0) {
-                closestUnit.killUnit();
-            }
-            //console.log(closestUnit.health);
-        }
-    }
-
-
-
+    //if(nightsAttacking) {
+        teamAttack(allUnits, 0)
+    //} else {
+        teamAttack(allUnits, 1)
+    //}
     /* Attack Logic end */
 }
 
